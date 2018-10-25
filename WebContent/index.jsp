9199a0eac11e20a5f0f316d0c57c1495bcddf2f6
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-10-25 02:53:59 -0500

    First commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Material icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+<!-- Materialize - Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="public/css/style.css">
+<title>MAE - Fruits</title>
+</head>
+<body>
+	<header>
+	</header>
+	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col s12 m12 l2 xl2">Dashboard</div>
+				<div class="col s12 m12 l8 xl8">Main content</div>
+			</div>
+		</div>
+	</main>
+	<footer>
+	
+	</footer>
+
+	<!-- Materialize - Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<script src="public/js/index.js"></script>
+</body>
+</html>
---
+++ src/controllers/FruitServlet.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/fruits")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public FruitServlet() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
